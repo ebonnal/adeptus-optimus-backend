6eeab365d5cbdb12b32b1bb7ea4ca7ad369f7fb2
EnzoBnl <enzobonnal@gmail.com>
2020-10-25 16:43:19 +0100

    engine v3 opti

--- adeptus-optimus-app/enginev3.py
+++ adeptus-optimus-app/enginev3.py
@@ -29,22 +29,15 @@ class Cache:
     def __str__(self):
         return f"tries={self.tries}, hits={self.hits}, misses={self.tries - self.hits}"
 
-    def add(self, state, downstream):
-        """
-        Store a copy of downstream
-        """
+    def add(self, state, cached_unweighted_downstream):
         key = Cache._keyify(state)
-        res = self.dict.get(key, None)
-        if res is None:
-            self.dict[key] = downstream[:]
-        elif len(res) < len(downstream):
-            # print(downstream, "replaced", res)
-            self.dict[key] = downstream[:]
+        if self.dict.get(key, (0, 0))[0] < state.n_unsaved_wounds_left:
+            self.dict[key] = (state.n_unsaved_wounds_left, cached_unweighted_downstream)
 
     def get(self, state):
-        res = self.dict.get(Cache._keyify(state), None)
+        res = self.dict.get(Cache._keyify(state), (None, None))
         self.tries += 1
-        if res is not None:
+        if res[0] is not None:
             self.hits += 1
         return res
 
@@ -56,7 +49,7 @@ class Cache:
 
     @staticmethod
     def _keyify(state):
-        return f"{state.current_wound_n_damages_left},{state.remaining_target_wounds}"
+        return f"{state.current_wound_n_damages_left},{state.remaining_target_wounds},{state.n_unsaved_wounds_left}"
 
 
 class Node:
@@ -73,31 +66,6 @@ class Node:
         self.parents_states = parents_states
         self.children_states = children_states
 
-
-def element_wise_sum(*ls):
-    assert(all(map(lambda l: len(l) == len(ls[0]), ls)))
-    return list(map(lambda elems: sum(elems), zip(*ls)))
-
-
-def scalar_mult_list_elems(k, l):
-    assert(type(l) is list)
-    try:
-        k = float(k)
-    except:
-        raise AttributeError("k must be a number")
-    return list(map(lambda e: k * e, l))
-
-
-def scalar_add_list_elems(b, l):
-    assert(type(l) is list)
-    try:
-        b = float(b)
-    except:
-        raise AttributeError("k must be a number")
-    return list(map(lambda e: b + e, l))
-
-
-# TODO: make all the sub triangle cached, not only the one going from node X to leafs: more cache hits
 def compute_slained_figs_frac(state_):
     assert (isinstance(state_, State))
     assert (state_.remaining_target_wounds >= 0)
@@ -111,43 +79,38 @@ def compute_slained_figs_frac(state_):
         # additionnal damages are not propagated to other models
         state.current_wound_n_damages_left = 0
         downstream = compute_slained_figs_frac(state)
-        downstream = scalar_add_list_elems(1, downstream)
-        #downstream[0] += 1
-        return downstream   # upstream propagation of figs slained count
+        downstream += 1
+        return downstream  # upstream propagation of figs slained count
 
     last_model_injured_frac = 1 - state.remaining_target_wounds / Node.target_wounds
 
     if state.current_wound_n_damages_left == 0 and state.n_unsaved_wounds_left == 0:
         # leaf: no more damages to fnp no more wounds to consume or p(leaf) < threshold
         # portion of the last model injured
-        Node.cache.add(state, [last_model_injured_frac])
-        return [last_model_injured_frac]
+        Node.cache.add(state, last_model_injured_frac)
+        return last_model_injured_frac
     else:
         # test cache
-        #cached_downstream = None
-        cached_downstream = Node.cache.get(state)
-        if cached_downstream is not None and state.n_unsaved_wounds_left < len(cached_downstream):
+        # cached_downstream = None
+        cached_res_n_unsaved_wounds_left, cached_downstream = Node.cache.get(state)
+        if cached_downstream is not None and cached_res_n_unsaved_wounds_left >= state.n_unsaved_wounds_left:
             # use cached res if deep enough
-            relevant_cached_downstream = cached_downstream[len(cached_downstream) - state.n_unsaved_wounds_left - 1:]
-            #relevant_cached_downstream = cached_downstream[0:state.n_unsaved_wounds_left+1]
-            return relevant_cached_downstream
+            return cached_downstream
         else:
             if state.current_wound_n_damages_left == 0:
-                if cached_downstream is None or state.n_unsaved_wounds_left >= len(cached_downstream):
+                if cached_downstream is None or cached_res_n_unsaved_wounds_left < state.n_unsaved_wounds_left:
                     # consume a wound
                     # random doms handling
                     res = [
-                        scalar_mult_list_elems(
-                            prob_d,
-                            compute_slained_figs_frac(State(n_unsaved_wounds_left=state.n_unsaved_wounds_left - 1,
-                                                               current_wound_n_damages_left=d,
-                                                               n_figs_slained_so_far=state.n_figs_slained_so_far,
-                                                               remaining_target_wounds=state.remaining_target_wounds))
-                        )
-                        for d, prob_d in prob_by_roll_result(Node.weapon_d).items()]
-                    downstream = element_wise_sum(*res)
+                        prob_d *
+                        compute_slained_figs_frac(State(n_unsaved_wounds_left=state.n_unsaved_wounds_left - 1,
+                                                        current_wound_n_damages_left=d,
+                                                        n_figs_slained_so_far=state.n_figs_slained_so_far,
+                                                        remaining_target_wounds=state.remaining_target_wounds))
+                        for d, prob_d in prob_by_roll_result(Node.weapon_d).items()
+                    ]
+                    downstream = sum(res)
                     Node.cache.add(state, downstream)
-                    downstream.append(last_model_injured_frac)
                     return downstream
             else:
                 # FNP fail
@@ -162,16 +125,11 @@ def compute_slained_figs_frac(state_):
                                                         state.current_wound_n_damages_left - 1,
                                                         state.n_figs_slained_so_far,
                                                         state.remaining_target_wounds))
-                    downstream = element_wise_sum(
-                        scalar_mult_list_elems(1 - Node.fnp_fail_ratio, s),
-                        scalar_mult_list_elems(Node.fnp_fail_ratio, f)
-                    )
+                    downstream = (1 - Node.fnp_fail_ratio) * s + Node.fnp_fail_ratio * f
                 else:
-                    downstream = scalar_mult_list_elems(Node.fnp_fail_ratio, f)
+                    downstream = f
                 Node.cache.add(state, downstream)
                 return downstream
-
-
 
 
 def compute_slained_figs_ratios_per_unsaved_wound(weapon_d, target_fnp, target_wounds, n_unsaved_wounds_init=32):
@@ -196,17 +154,8 @@ def compute_slained_figs_ratios_per_unsaved_wound(weapon_d, target_fnp, target_w
         n_unsaved_wounds_left=n_unsaved_wounds_init,
         current_wound_n_damages_left=0,
         n_figs_slained_so_far=0,
-        remaining_target_wounds=target_wounds))[0] / Node.n_unsaved_wounds_init
+        remaining_target_wounds=target_wounds)) / Node.n_unsaved_wounds_init
 
-import enginev2, enginev3legacy
-print(with_timer(lambda: enginev2.compute_slained_figs_ratios_per_unsaved_wound(DiceExpr(1, 3), 6, 5, n_unsaved_wounds_init=3)))
-print(with_timer(lambda: enginev3legacy.compute_slained_figs_ratios_per_unsaved_wound(DiceExpr(1,3), 6, 5, n_unsaved_wounds_init=3)))
-print(enginev3legacy.Node.cache)
-print(enginev3legacy.Node.cache.dict)
-print(with_timer(lambda: compute_slained_figs_ratios_per_unsaved_wound(DiceExpr(1,3), 6, 5, n_unsaved_wounds_init=3)))
-print(Node.cache)
-print(Node.cache.dict)
-exit(0)
 # FNP
 assert (float_eq(compute_slained_figs_ratios_per_unsaved_wound(DiceExpr(1), 6, 1), 5 / 6, 0))
 assert (float_eq(compute_slained_figs_ratios_per_unsaved_wound(DiceExpr(1), 5, 1), 4 / 6, 0))
@@ -218,12 +167,12 @@ assert (float_eq(compute_slained_figs_ratios_per_unsaved_wound(DiceExpr(2, 3), N
 # random doms
 assert (float_eq(compute_slained_figs_ratios_per_unsaved_wound(DiceExpr(1, 6), None, 35), 0.1, 0))
 assert (float_eq(compute_slained_figs_ratios_per_unsaved_wound(
-DiceExpr(1, 6), 4, 175), 0.01, 0)
+    DiceExpr(1, 6), 4, 175), 0.01, 0)
 )
 
 assert (float_eq(compute_slained_figs_ratios_per_unsaved_wound(
-DiceExpr(1, 6), 5, 70, n_unsaved_wounds_init=70), 2 / 3 * 3.5 / 70, 0)
+    DiceExpr(1, 6), 5, 70, n_unsaved_wounds_init=70), 2 / 3 * 3.5 / 70, 0)
 )
 # lost damages
 assert (float_eq(compute_slained_figs_ratios_per_unsaved_wound(DiceExpr(5), target_fnp=None, target_wounds=6,
-                                                           n_unsaved_wounds_init=33), 0.5, 0))
+                                                               n_unsaved_wounds_init=33), 0.5, 0))
